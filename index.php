3663436d0fb54b50510b1da575945343d5a66929
lauratita <lauratita316@gmail.com>
2023-01-29 10:59:22 +0700

    first file OOP

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// class laptop
+class laptop
+{
+    // property untuk class laptop
+    var $pemilik;
+    var $merk;
+    var $ukuran_layar;
+
+    // method unruk calss laptop
+    function hidupkan_laptop()
+    {
+        return "Hidupkan Laptop";
+    }
+    function matikan_laptop()
+    {
+        return "Matikan Laptop";
+    }
+}
+
+// objek dari class laptop (instansiasi)
+$laptop_anto = new laptop();
+
+// set property
+$laptop_anto->pemilik = "Anto";
+$laptop_anto->merk = "Asus";
+$laptop_anto->ukuran_layar = "15 inchi";
+
+// tampilkan property
+echo $laptop_anto->pemilik;
+echo "<br />";
+echo $laptop_anto->merk;
+echo "<br />";
+echo $laptop_anto->ukuran_layar;
+echo "<br />";
+
+// tampilkan method
+echo $laptop_anto->hidupkan_laptop();
+echo "<br />";
+echo $laptop_anto->matikan_laptop();
